a8cb69e2e740d4554b209ca958e9d78f55d42170
TC Wang <wangtct@hotmail.com>
2024-01-02 11:27:04 +0800

    unify

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="container">
-    <div>地方行政</div>
+    <h1>A7新林口-社區網站: 14.地方行政資訊平台</h1>
   </div>
 </template>
 
-<script></script>
+<script setup>
+useHead({
+  title: "A7-14.地方行政",
+  meta: [
+    { name: "description", content: "A7新林口-社區網站: 14.地方行政資訊平台 " },
+  ],
+});
+</script>
 
 <style></style>
