33b58e2ec407601624b2fbdf8761808802913b00
TC Wang <wangtct@hotmail.com>
2024-01-03 08:12:50 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
     <h1>A7新林口-社區網站: 14.地方行政資訊平台</h1>
+    <NuxtImg
+      src="https://kgptltaqnkidtcqkathb.supabase.co/storage/v1/object/public/a7-images/announcement/ANN-000.png"
+      class="w-full"
+    />
   </div>
 </template>
 
