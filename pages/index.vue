577113a52028a6fb3c2f6909ad24417f52a1488b
TC Wang <wangtct@hotmail.com>
2024-02-15 10:43:12 +0800

    home

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
     <h1>A7新林口-社區網站: 14.地方行政資訊平台</h1>
+
+    福利智慧雲！ 讓桃園居民福利不漏接
+
+    <a href="https://welfarecloud.tycg.gov.tw/">
+      <NuxtImg src="/images/announcement/a0001-1.png" />
+    </a>
   </div>
 </template>
 
