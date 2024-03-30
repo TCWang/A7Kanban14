a5978d105934c97c1fa64a426e705eb814348e01
TC Wang <wangtct@hotmail.com>
2024-03-31 04:34:35 +0800

    競選政見匯總

--- pages/community/index.vue
+++ pages/community/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>里長交流</h1>
+    <a :href="`/community/announcement/a0001`">
+      <button
+        class="h-10 px-4 rounded-md text-zinc-800 bg-lime-500 hover:bg-lime-700 hover:text-white duration-500"
+      >
+        里長競選政見匯總
+      </button>
+    </a>
     <CommunityTable />
   </div>
 </template>
--- pages/councilor/index.vue
+++ pages/councilor/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>民意代表 - 龜山區議員 / 區域立委</h1>
+    <a :href="`/councilor/service/s0001`">
+      <button
+        class="h-10 px-4 rounded-md text-zinc-800 bg-lime-500 hover:bg-lime-700 hover:text-white duration-500"
+      >
+        議員競選政見匯總
+      </button>
+    </a>
 
     <CouncilorTable />
     <CouncilorSelection />
