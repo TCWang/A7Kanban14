b85f259a44d67ca1ab81ba0d79f1f508e3725cd4
TC Wang <wangtct@hotmail.com>
2024-05-28 14:21:28 +0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,12 @@
   <div class="container">
     <h1>A7新林口-社區網站: 14.地方行政資訊平台</h1>
 
-    福利智慧雲！ 讓桃園居民福利不漏接
-
-    <a href="https://welfarecloud.tycg.gov.tw/">
-      <NuxtImg src="/images/announcement/a0001-1.png" />
-    </a>
+    遠見 2024:
+    <a href="https://www.gvm.com.tw/article/113001">五星縣市長揭曉</a>
+    <div class="flex flex-row flex-wrap">
+      <NuxtImg class="w-full md:w-1/2" src="/images/announcement/a0001-2.png" />
+      <NuxtImg class="w-full md:w-1/2" src="/images/announcement/a0001-3.png" />
+    </div>
   </div>
 </template>
 
