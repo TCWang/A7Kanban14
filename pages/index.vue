d90bad8e336dde1b21cb2a15816b0a13890cc0ac
TC Wang <wangtct@hotmail.com>
2024-12-23 22:01:31 +0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,62 @@
 <template>
   <div class="">
-    <h1>A7新林口-社區網站: 14.地方行政資訊平台</h1>
-    <h3>中時新聞網 2024/12/23</h3>
-    <nl>
-      <li>
-        中時新聞網 2024/12/23:<a
-          href="https://www.chinatimes.com/realtimenews/20241223001576-260407?utm_source=push&utm_medium=image&chdtv"
-        >
-          71.5％肯定 張善政最新民調超驚人！3大政策滿意度破8成</a
-        >
-      </li>
-    </nl>
+    <div class="w-full md:w-1/2">
+      <h1>A7新林口-社區網站: 14.地方行政資訊平台</h1>
+      <h3>中時新聞網 2024/12/23</h3>
+      <nl>
+        <li>
+          中時新聞網 2024/12/23:<a
+            href="https://www.chinatimes.com/realtimenews/20241223001576-260407?utm_source=push&utm_medium=image&chdtv"
+          >
+            71.5％肯定 張善政最新民調超驚人！3大政策滿意度破8成</a
+          >
+        </li>
+      </nl>
 
-    <h3>TVBS 2024/12/11</h3>
-    <nl>
-      <li>
-        <a
-          href="https://newtalk.tw/news/view/2024-12-13/948855?fbclid=IwY2xjawHMW0JleHRuA2FlbQIxMQABHWahDhP-zPSQ1t7jyw8ZBfkz3O7hJJBRuOb-V4wy9Fuizg_nwZYaWJkBCA_aem_m2dZ2OP1dfajxZbFD8IzBw"
-          >六都滿意度排名 盧秀燕、陳其邁破6成分居1、2</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://news.tvbs.com.tw/politics/2714672?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWwZleHRuA2FlbQIxMAABHbDdkcXcvwsg1F4uXIOQa_VOgLd9-OMVOgXULpnYPSh33Pzhfg32_QEpWw_aem_iQLUgmSl4wDq5P_5rqP3VA"
-          >張善政施政滿意度破5成　上任2年以來最好</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://news.tvbs.com.tw/politics/2714680?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWYNleHRuA2FlbQIxMAABHUz1VxVBqaijJO5t5d7cSJom92JiNyx_WGd-WLIDTpJnzsze_QNWCSOR1Q_aem_End4M6R9EvO_vMyX1ulGPQ"
-          >破7成市民以住桃園市為榮　6成5「覺得快樂」</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://news.tvbs.com.tw/politics/2715184?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWdxleHRuA2FlbQIxMAABHeaNmpb8F3D9X7A9y4fY9ZCakEjVxUwWsa3-7U4lf6nunxxNztkAzTPsFw_aem_94k26FgL1DBPoQO7aRA9Pw"
-          >張善政滿意度上任最高　中壢飆破6成、僅「這區」下滑</a
-        >
-      </li>
-    </nl>
+      <br />
 
-    <br />
+      <p>
+        根據《ETtoday民調雲》今公布該份民調結果，有71.5%的桃園市對於桃園市長張善政的施政表現表示滿意，不滿意者為18.3%，10.3%的民眾表示不知道或沒意見。另外，對於桃園市府推動的智慧治理政策（將AI與物聯網等技術應用於市政管理）、經濟招商、推動捷運路網建設，都有破8成民眾表示滿意。
+      </p>
+      <p>
+        至於在生活滿意度部分，有85.6%的市民表示居住在桃園市讓他們感到幸福，認為不幸福者為10.7%。
+      </p>
+      <p>
+        該民調調查時間為12月9日至12月13日，調查範圍及對象是設籍桃園市年滿20歲以上的民眾，以EDM方式進行「城市發展與公共建設」網路問卷調查，有效樣本數2,227份，在95%信心水準下，誤差為±2.08%。
+      </p>
+
+      <br />
+
+      <h3>TVBS 2024/12/11</h3>
+      <nl>
+        <li>
+          <a
+            href="https://newtalk.tw/news/view/2024-12-13/948855?fbclid=IwY2xjawHMW0JleHRuA2FlbQIxMQABHWahDhP-zPSQ1t7jyw8ZBfkz3O7hJJBRuOb-V4wy9Fuizg_nwZYaWJkBCA_aem_m2dZ2OP1dfajxZbFD8IzBw"
+            >六都滿意度排名 盧秀燕、陳其邁破6成分居1、2</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://news.tvbs.com.tw/politics/2714672?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWwZleHRuA2FlbQIxMAABHbDdkcXcvwsg1F4uXIOQa_VOgLd9-OMVOgXULpnYPSh33Pzhfg32_QEpWw_aem_iQLUgmSl4wDq5P_5rqP3VA"
+            >張善政施政滿意度破5成　上任2年以來最好</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://news.tvbs.com.tw/politics/2714680?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWYNleHRuA2FlbQIxMAABHUz1VxVBqaijJO5t5d7cSJom92JiNyx_WGd-WLIDTpJnzsze_QNWCSOR1Q_aem_End4M6R9EvO_vMyX1ulGPQ"
+            >破7成市民以住桃園市為榮　6成5「覺得快樂」</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://news.tvbs.com.tw/politics/2715184?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWdxleHRuA2FlbQIxMAABHeaNmpb8F3D9X7A9y4fY9ZCakEjVxUwWsa3-7U4lf6nunxxNztkAzTPsFw_aem_94k26FgL1DBPoQO7aRA9Pw"
+            >張善政滿意度上任最高　中壢飆破6成、僅「這區」下滑</a
+          >
+        </li>
+      </nl>
+
+      <br />
+    </div>
 
     <div class="flex flex-row flex-wrap">
       <NuxtImg
