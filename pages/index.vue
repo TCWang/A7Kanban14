5c6aa8aeacda45ac375c41f224209c15d031f547
TC Wang <wangtct@hotmail.com>
2025-05-28 14:42:47 +0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,123 @@
 <template>
-  <div class="">
-    <div class="flex flex-row flex-wrap">
-      <div class="w-full md:w-1/2">
+  <div class="space-y-6">
+    <!-- Section 1: 遠見雜誌 -->
+    <div class="flex flex-wrap">
+      <div class="w-full md:w-1/2 p-2">
         <h1>A7新林口-社區網站: 14.地方行政資訊平台</h1>
-        <h3>2025 風傳媒民調</h3>
+        <h3>2025 遠見雜誌民調</h3>
         <a
-          href="https://www.storm.mg/articles/1039802?utm_source=fb_news&utm_medium=post&fbclid=IwY2xjawKcPlRleHRuA2FlbQIxMABicmlkETFBQVNtaGlUZFFBb0FiSGt4AR5pTz3G6cnScG_mU71k8Tk87Xd66eqQKFoyEEhpfGs5ld2I9d1yR4hcKrHLMg_aem_a_5eZ-HZwuXaV_YTD48B9w"
-          >風傳媒 2025/05/21:</a
+          href="https://www.gvm.com.tw/article/121594?fbclid=..."
+          target="_blank"
         >
-        張善政施政滿意度80%六都居冠,
-        後依序為，高雄市長陳其邁79.6%緊隨其後；台中市長盧秀燕73.1%名列第三；新北市長侯友宜71.2%、台南市長黃偉哲70.3%，台北市長蔣萬安63.2%，為六都最後一名。
-        <br /><br />
-        五大面向政策方面，「婦幼政策」調查結果顯示，桃園市以79.5%的滿意度排名第一，後依序為新北市60.5%、高雄市57.5%、台中市57.1%、台南市52.4%、台北市48.6%。「改善交通和加強大眾運輸」部分，高雄市以71.3%的滿意度排名第一，後序為台北市67.8%、新北市64.7%、台中市62.7%、桃園市60.1%、台南市56.0%。
-        <br /><br />
-        「推動環保維護環境品質」調查結果顯示，桃園市以80.6%的滿意度排名第一，後依序為，新北市79.2%、台北市73.0%、高雄市71.8%、台南市71.5%、台中市70.4%。「整頓市容部分」，高雄市以79.2%的滿意度排名第一，其次依序為，桃園市72.1%、台中市
-        71.5%、新北市66.2%、台北市61.8%、台南市61.5%。「公共醫療」部分，桃園市以85.1%的滿意度排名第一，後依序為高雄市76.3%、新北市74.7%、台中市74.0%、台北市72.4%、台南市70.1%。
-        <br /><br />
+          遠見雜誌 2025/05/27:
+        </a>
+        <p>
+          縣市長施政滿意度調查終於出爐，今年奪下五星的首長，包括：
+          陳其邁、張麗善、翁章梁、黃敏惠、饒慶鈴。滿意度躍升最多、
+          最佳進步表現獎則有謝國樑、黃偉哲。施政總體檢前三強為嘉義縣、嘉義市、高雄市。
+        </p>
+        <NuxtImg class="w-full" src="/images/announcement/a0001-14.jpeg" />
       </div>
-
-      <NuxtImg
-        class="w-full md:w-1/2 p-1"
-        src="/images/announcement/a0001-13.jpeg"
-      />
+      <div class="w-full md:w-1/2 p-2">
+        <NuxtImg class="w-full" src="/images/announcement/a0001-15.jpeg" />
+      </div>
     </div>
 
-    <div class="w-full md:w-1/2">
+    <!-- Section 2: 風傳媒 -->
+    <div class="flex flex-wrap">
+      <div class="w-full md:w-1/2 p-2">
+        <h3>2025 風傳媒民調</h3>
+        <a href="https://www.storm.mg/articles/1039802?..." target="_blank">
+          風傳媒 2025/05/21:
+        </a>
+        <p>
+          張善政施政滿意度80%六都居冠，後依序為：陳其邁、盧秀燕、侯友宜、黃偉哲、蔣萬安。
+          <br /><br />
+          「婦幼政策」第一為桃園市79.5%，再為新北、高雄、台中、台南、台北。
+          <br />
+          「改善交通」第一為高雄市71.3%，再為台北、新北、台中、桃園、台南。
+          <br />
+          「推動環保」第一為桃園市80.6%，其次為新北、台北、高雄、台南、台中。
+          <br />
+          「整頓市容」第一為高雄市79.2%，其次為桃園、台中、新北、台北、台南。
+          <br />
+          「公共醫療」第一為桃園市85.1%，再為高雄、新北、台中、台北、台南。
+        </p>
+      </div>
+      <div class="w-full md:w-1/2 p-2">
+        <NuxtImg class="w-full" src="/images/announcement/a0001-13.jpeg" />
+      </div>
+    </div>
+
+    <!-- Section 3: 中時新聞網 & TVBS -->
+    <div class="w-full md:w-1/2 p-2">
       <h3>中時新聞網 2024/12/23</h3>
-      <nl>
+      <ul class="list-disc ml-5">
         <li>
-          中時新聞網 2024/12/23:<a
-            href="https://www.chinatimes.com/realtimenews/20241223001576-260407?utm_source=push&utm_medium=image&chdtv"
+          <a
+            href="https://www.chinatimes.com/realtimenews/20241223001576-260407?utm_source=..."
+            target="_blank"
           >
-            71.5％肯定 張善政最新民調超驚人！3大政策滿意度破8成</a
-          >
+            71.5％肯定 張善政最新民調超驚人！3大政策滿意度破8成
+          </a>
         </li>
         <li>
-          ETtoday 2024/12/23:<a
+          <a
             href="https://www.ettoday.net/news/20241223/2876786.htm"
-            >ET民調／71.5%桃園市民肯定張善政表現　五大政策滿意度破8成</a
+            target="_blank"
           >
+            ET民調／71.5%桃園市民肯定張善政表現　五大政策滿意度破8成
+          </a>
         </li>
-      </nl>
-
-      <br />
-
+      </ul>
       <p>
-        根據《ETtoday民調雲》今公布該份民調結果，有71.5%的桃園市對於桃園市長張善政的施政表現表示滿意，不滿意者為18.3%，10.3%的民眾表示不知道或沒意見。另外，對於桃園市府推動的智慧治理政策（將AI與物聯網等技術應用於市政管理）、經濟招商、推動捷運路網建設，都有破8成民眾表示滿意。
+        根據《ETtoday民調雲》，71.5%的桃園市民對市長張善政施政表示滿意。
+        <br />
+        智慧治理、招商、捷運建設滿意度均破8成。
       </p>
       <p>
-        至於在生活滿意度部分，有85.6%的市民表示居住在桃園市讓他們感到幸福，認為不幸福者為10.7%。
+        居住幸福感85.6%，調查期間：2024/12/09 至 2024/12/13，有效樣本數2,227。
       </p>
-      <p>
-        該民調調查時間為12月9日至12月13日，調查範圍及對象是設籍桃園市年滿20歲以上的民眾，以EDM方式進行「城市發展與公共建設」網路問卷調查，有效樣本數2,227份，在95%信心水準下，誤差為±2.08%。
-      </p>
-
-      <br />
 
       <h3>TVBS 2024/12/11</h3>
-      <nl>
+      <ul class="list-disc ml-5">
         <li>
           <a
-            href="https://newtalk.tw/news/view/2024-12-13/948855?fbclid=IwY2xjawHMW0JleHRuA2FlbQIxMQABHWahDhP-zPSQ1t7jyw8ZBfkz3O7hJJBRuOb-V4wy9Fuizg_nwZYaWJkBCA_aem_m2dZ2OP1dfajxZbFD8IzBw"
-            >六都滿意度排名 盧秀燕、陳其邁破6成分居1、2</a
+            href="https://newtalk.tw/news/view/2024-12-13/948855?fbclid=..."
+            target="_blank"
           >
+            六都滿意度排名 盧秀燕、陳其邁破6成分居1、2
+          </a>
         </li>
         <li>
           <a
-            href="https://news.tvbs.com.tw/politics/2714672?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWwZleHRuA2FlbQIxMAABHbDdkcXcvwsg1F4uXIOQa_VOgLd9-OMVOgXULpnYPSh33Pzhfg32_QEpWw_aem_iQLUgmSl4wDq5P_5rqP3VA"
-            >張善政施政滿意度破5成　上任2年以來最好</a
+            href="https://news.tvbs.com.tw/politics/2714672?..."
+            target="_blank"
           >
+            張善政施政滿意度破5成　上任2年以來最好
+          </a>
         </li>
         <li>
           <a
-            href="https://news.tvbs.com.tw/politics/2714680?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWYNleHRuA2FlbQIxMAABHUz1VxVBqaijJO5t5d7cSJom92JiNyx_WGd-WLIDTpJnzsze_QNWCSOR1Q_aem_End4M6R9EvO_vMyX1ulGPQ"
-            >破7成市民以住桃園市為榮　6成5「覺得快樂」</a
+            href="https://news.tvbs.com.tw/politics/2714680?..."
+            target="_blank"
           >
+            破7成市民以住桃園市為榮　6成5「覺得快樂」
+          </a>
         </li>
         <li>
           <a
-            href="https://news.tvbs.com.tw/politics/2715184?&utm_source=facebook&utm_medium=fbarticlecomments&fbclid=IwY2xjawHMWdxleHRuA2FlbQIxMAABHeaNmpb8F3D9X7A9y4fY9ZCakEjVxUwWsa3-7U4lf6nunxxNztkAzTPsFw_aem_94k26FgL1DBPoQO7aRA9Pw"
-            >張善政滿意度上任最高　中壢飆破6成、僅「這區」下滑</a
+            href="https://news.tvbs.com.tw/politics/2715184?..."
+            target="_blank"
           >
+            張善政滿意度上任最高　中壢飆破6成、僅「這區」下滑
+          </a>
         </li>
-      </nl>
-
-      <br />
+      </ul>
     </div>
 
-    <div class="flex flex-row flex-wrap">
+    <!-- Section 4: Images -->
+    <div class="flex flex-wrap">
       <NuxtImg
         class="w-full md:w-1/2 p-1"
         src="/images/announcement/a0001-12.png"
@@ -97,8 +127,7 @@
         src="/images/announcement/a0001-11.png"
       />
     </div>
-
-    <div class="flex flex-row flex-wrap">
+    <div class="flex flex-wrap">
       <NuxtImg
         class="w-full md:w-1/2 p-1"
         src="/images/announcement/a0001-5.png"
@@ -108,8 +137,7 @@
         src="/images/announcement/a0001-7.png"
       />
     </div>
-
-    <div class="flex flex-row flex-wrap">
+    <div class="flex flex-wrap">
       <NuxtImg
         class="w-full md:w-1/2 p-1"
         src="/images/announcement/a0001-8.png"
@@ -120,23 +148,33 @@
       />
     </div>
 
+    <!-- Section 5: 遠見補充 -->
     <h2>遠見 2024/05/27:</h2>
-    <a href="https://www.gvm.com.tw/article/113001">五星縣市長揭曉</a>
-    <NuxtImg class="w-full md:w-3/4" src="/images/announcement/a0001-4.png" />
-    <div class="flex flex-row flex-wrap">
-      <NuxtImg class="w-full md:w-1/2" src="/images/announcement/a0001-2.png" />
-      <NuxtImg class="w-full md:w-1/2" src="/images/announcement/a0001-3.png" />
+    <a href="https://www.gvm.com.tw/article/113001" target="_blank"
+      >五星縣市長揭曉</a
+    >
+    <NuxtImg
+      class="w-full md:w-3/4 mx-auto"
+      src="/images/announcement/a0001-4.png"
+    />
+    <div class="flex flex-wrap">
+      <NuxtImg
+        class="w-full md:w-1/2 p-1"
+        src="/images/announcement/a0001-2.png"
+      />
+      <NuxtImg
+        class="w-full md:w-1/2 p-1"
+        src="/images/announcement/a0001-3.png"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
-import { normalizeClass } from "vue";
-
 useHead({
   title: "A7-14.地方行政",
   meta: [
-    { name: "description", content: "A7新林口-社區網站: 14.地方行政資訊平台 " },
+    { name: "description", content: "A7新林口-社區網站: 14.地方行政資訊平台" },
   ],
 });
 </script>
